7d66e304ede176674f64c9205743acbe96e889dc
babinm1h <babinmih@mail.ru>
2023-12-23 15:10:14 +0300

    rq

--- src/core/containers/Router/Router.tsx
+++ src/core/containers/Router/Router.tsx
@@ -1,12 +1,14 @@
 import { ProductsPage } from '@/modules/Products/containers';
 import { Route, Routes } from 'react-router-dom';
 import { BaseLayout } from '../BaseLayout/BaseLayout';
+import { UsersPage } from '@/modules/Users/containers';
 
 export const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<BaseLayout />}>
         <Route element={<ProductsPage />} index />
+        <Route element={<UsersPage />} path="/users" />
       </Route>
     </Routes>
   );
